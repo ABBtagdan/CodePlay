e990e8b0e5da2cc4b86f68504dbf9dd5d5fb4b06
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-11 19:22:19 +0100

    For test deployment

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import barcode
 from flask import Flask, request, Response
-import time
-import os
+import flask
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
@@ -29,7 +28,9 @@ def image():
     return Response(seq, status=200, content_type="text/plain")
 
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
+    
+    
 
     
 
