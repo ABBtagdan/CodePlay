304abfb650491ee006fbd2fc2f4e8e488b15fde1
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-17 13:59:57 +0100

    small change

--- playMusic.js
+++ playMusic.js
@@ -1,7 +1,5 @@
 // Olle Ö
 
-let musicList = ['we-live-we-love-we-lie-made-with-Voicemod-technology.mp3', 'Find_Your_One_Way.mp3', 'Play_the_Hero.mp3', 'Operation_Pyrite.mp3']
-
 let i = 0
 
 function play (musicList) {
