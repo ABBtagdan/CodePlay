dce01398865997f53b7a5d875017dd1e4c9fc15b
abbaxeros <axel.rosenberg@abbgymnasiet.se>
2024-01-17 13:27:50 +0100

    added module

---
+++ convertToSound.py
@@ -0,0 +1,27 @@
+stringInput = ""
+
+#stringInput = input("\n Enter a series of integers from 0 to 4.  --->  ")
+#print(" Your chosen is series is as follows: "+ stringInput +"\n")
+"""    // The lines above are exclusively used for testing. 
+          'stringInput' can receive string from any source.   """
+
+stringList = list(stringInput)
+audioList = []
+#print(stringList)      #    // Debugging purposes
+
+for digit in stringList:
+    if digit == "0":
+        currentDirectory = "./sounds/doo"
+    elif digit =="1":
+        currentDirectory = "./sounds/ree"
+    elif digit =="2":
+        currentDirectory = "./sounds/mii"
+    elif digit =="3":
+        currentDirectory = "./sounds/faa"
+    elif digit =="4":
+        currentDirectory = "./sounds/sol"
+    else:
+        print ("oh no") #    // Debugging purposes - Received only upon incorrect input
+    audioList.append(currentDirectory)
+
+#print(audioList)        #    // Debugging purposes
