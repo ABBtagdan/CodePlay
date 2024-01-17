ba3580db3afae9ff161e7212a5bbf14a9ce65085
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-17 14:33:27 +0100

    fix

--- playMusic.js
+++ playMusic.js
@@ -3,7 +3,7 @@
 let i = 0
 
 function play (musicList) {
-
+    console.log(musicList)
     if (i < musicList.length) {
         let aud = document.getElementById('audio')
         aud.src = musicList[i] + '.mp3'
@@ -11,7 +11,7 @@ function play (musicList) {
         aud.play()
         i++
         aud.addEventListener("ended", function () {
-            play()
+            play(musicList)
         })
     }
     else {
