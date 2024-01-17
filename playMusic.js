9fe98829032041247fc9be1f096e47140429d8a9
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-17 14:31:56 +0100

    added debog log

--- playMusic.js
+++ playMusic.js
@@ -7,6 +7,7 @@ function play (musicList) {
     if (i < musicList.length) {
         let aud = document.getElementById('audio')
         aud.src = musicList[i] + '.mp3'
+        console.log("playing "+musicList[i]+".mp3")
         aud.play()
         i++
         aud.addEventListener("ended", function () {
