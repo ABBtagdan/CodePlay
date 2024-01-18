6f237d1b59c3dfc9ed2375780e2b42d52b55fb9a
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-18 14:44:57 +0100

    try 15

--- playMusic.js
+++ playMusic.js
@@ -19,9 +19,9 @@ function play () {
 
         aud.load()
         // console.log("playing "+musicList[i]+".mp3")
-        aud.play().catch(function (error) {
-            console.error('Error during playback:', error.message)
-        })
+        // aud.play().catch(function (error) {
+        //     console.error('Error during playback:', error.message)
+        // })
         i++
         setTimeout(function () {
             play()
@@ -31,5 +31,11 @@ function play () {
         i = 0
     }
 }
+
+aud.addEventListener('canplaythrough', function () {
+    aud.play().catch(function (error) {
+        console.error('Error during playback:', error.message);
+    });
+})
 
 // aud.addEventListener("ended", ()=>{if (i != 0){play()}})
