74e9e4a3501022bf597ce7116fb7c49e16b5d9d4
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-22 12:28:18 +0100

    used one file instead

--- create_song.py
+++ create_song.py
@@ -3,10 +3,13 @@
 from pydub import AudioSegment
 import os
 
+soundrange = {"doo": (1000, 1250), "ree": (750, 1000), "mii": (500, 750), "faa": (250, 500), "sol": (0,250)}
+
 def concat_sound(sound_list):
-    export = AudioSegment.from_mp3(sound_list[0]+".mp3")[:250]
+    uncut_audio = AudioSegment.from_mp3("./sounds/Drums.mp3")
+    export = uncut_audio[soundrange[sound_list[0][-3:]][0]:soundrange[sound_list[0][-3:]][1]]
     for s in sound_list[1:]:
-        export += AudioSegment.from_mp3(s+".mp3")[:250]
+        export += uncut_audio[soundrange[s[-3:]][0]:soundrange[s[-3:]][1]]
     os.remove("./sounds/temp.mp3")
     export.export("./sounds/temp.mp3")
     return ["./sounds/temp"]
