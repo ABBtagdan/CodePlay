a26d03e02c594b88a26d097f6ba72a3d173f36e5
abbtagdan <tage.danielsson@hitachigymnasiet.se>
2024-01-22 13:07:16 +0100

    no longer needed since list of sounds are of length one

--- create_song.py
+++ create_song.py
@@ -9,6 +9,7 @@ def concat_sound(sound_list):
     uncut_audio = AudioSegment.from_mp3("./sounds/Drums.mp3")
     export = uncut_audio[soundrange[sound_list[0][-3:]][0]:soundrange[sound_list[0][-3:]][1]]
     for s in sound_list[1:]:
+        print(s[-3:])
         export += uncut_audio[soundrange[s[-3:]][0]:soundrange[s[-3:]][1]]
     os.remove("./sounds/temp.mp3")
     export.export("./sounds/temp.mp3")
--- playMusic.js
+++ playMusic.js
@@ -12,30 +12,13 @@ function init (ml) {
 
 function play () {
     // console.log(musicList)
-    if (i < musicList.length) {
-        aud.pause()
-
-        aud.src = musicList[i] + '.mp3'
-
-        aud.load()
+    aud.pause()
+    aud.src = musicList[i] + '.mp3'
+    aud.load()
         // console.log("playing "+musicList[i]+".mp3")
         // aud.play().catch(function (error) {
         //     console.error('Error during playback:', error.message)
         // })
-        i++
-    }
-    else {
-        i = 0
-    }
 }
-
-aud.addEventListener('canplaythrough', function () {
-    aud.play().catch(function (error) {
-        console.error('Error during playback:', error.message);
-    });
-    setTimeout(function () {
-        play()
-    }, 125)
-})
 
 // aud.addEventListener("ended", ()=>{if (i != 0){play()}})
