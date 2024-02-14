2f0907395aeb91db72b6c8f0492cc647c5df32a0
ABB22OllOst <olle.ostman@abbgymnasiet.se>
2024-02-14 13:51:18 +0100

    comment

--- recording.js
+++ recording.js
@@ -1,71 +1,72 @@
 // Olle Ö
 
-record = document.getElementById("record");
-listen = document.getElementById("listen");
-satisfied = document.getElementById("satisfied");
-recordedAudio = document.getElementById("recordedAudio");
+// Get buttons from html
+record = document.getElementById("record")
+listen = document.getElementById("listen")
+satisfied = document.getElementById("satisfied")
+recordedAudio = document.getElementById("recordedAudio")
 
 // Makes it posible to record audio
 navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-  handlerFunction(stream);
-});
+    handlerFunction(stream)
+})
 
-let i = 0;
+let i = 0
 
-let blob;
+let blob
 
-function handlerFunction(stream) {
-  console.log("Got media device");
-  rec = new MediaRecorder(stream);
-  rec.ondataavailable = (e) => {
-    audioChunks.push(e.data);
-    if (rec.state == "inactive") {
-      blob = new Blob(audioChunks, { type: "audio/wav" });
-      recordedAudio.src = URL.createObjectURL(blob);
-      recordedAudio.controls = false;
-      recordedAudio.autoplay = true;
-      sendData(blob);
+function handlerFunction (stream) {
+    console.log("Got media device")
+    rec = new MediaRecorder(stream)
+    rec.ondataavailable = (e) => {
+        audioChunks.push(e.data)
+        if (rec.state == "inactive") {
+            blob = new Blob(audioChunks, { type: "audio/wav" })
+            recordedAudio.src = URL.createObjectURL(blob)
+            recordedAudio.controls = false
+            recordedAudio.autoplay = true
+            sendData(blob)
+        }
     }
-  };
 }
 
-function sendData(data) {}
+function sendData (data) { }
 // To start the recording and stops after 4 recordings
 record.onclick = (e) => {
-  record.disabled = true;
-  satisfied.disabled = true;
-  listen.disabled = true;
-  audioChunks = [];
-  if (i == 0) {
-    rec.start();
-  } else {
-    rec.resume();
-  }
-  setTimeout(function () {
-    i++;
-    record.disabled = false;
-    if (i == 4) {
-      satisfied.disabled = false;
-      listen.disabled = false;
-      rec.stop();
-      i = 0;
+    record.disabled = true
+    satisfied.disabled = true
+    listen.disabled = true
+    audioChunks = []
+    if (i == 0) {
+        rec.start()
     } else {
-      rec.pause();
+        rec.resume()
     }
-  }, 1000);
-};
+    setTimeout(function () {
+        i++
+        record.disabled = false
+        if (i == 4) {
+            satisfied.disabled = false
+            listen.disabled = false
+            rec.stop()
+            i = 0
+        } else {
+            rec.pause()
+        }
+    }, 1000)
+}
 
 // Plays back the sound you just recorded
 listen.onclick = (e) => {
-  recordedAudio.play();
-};
+    recordedAudio.play()
+}
 
 // Sends of the finished recording to the server
 satisfied.onclick = (e) => {
-  // makeRec(recordedAudio.src)
-  var xmlhttp = new XMLHttpRequest();
-  var url = "https://b862-13-48-25-29.ngrok-free.app//recording";
-  xmlhttp.open("POST", url, true);
-  xmlhttp.setRequestHeader("Content-type", "audio/wav");
-  xmlhttp.send(blob);
-};
+    // makeRec(recordedAudio.src)
+    var xmlhttp = new XMLHttpRequest()
+    var url = "https://b862-13-48-25-29.ngrok-free.app//recording"
+    xmlhttp.open("POST", url, true)
+    xmlhttp.setRequestHeader("Content-type", "audio/wav")
+    xmlhttp.send(blob)
+}
